c625f33764cf00e2ad8af57938a49110c5bed03b
Francis Tiangco <fptiangco@gmail.com>
2022-03-11 17:51:43 +0000

    Add github url

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     description="String tools",
     long_description=README_MD,
     long_description_content_type="text/markdown",
+    url="https://github.com/fptiangco/stringtoolsfpat",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",

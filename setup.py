d8b404511f745e90378124d6a09ea8629c4ab044
Francis Tiangco <fptiangco@gmail.com>
2022-03-11 21:40:43 +0000

    Add download url

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="stringtoolsfpat",
-    version="0.0.9",
+    version="0.0.10",
     packages=find_packages(),
     description="String tools",
     long_description=README_MD,
     long_description_content_type="text/markdown",
     url="https://github.com/fptiangco/stringtoolsfpat",
+    download_url="https://github.com/fptiangco/stringtoolsfpat",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",

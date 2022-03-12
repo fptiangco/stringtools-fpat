66865a3725f7df9a72cab362bc6265f555c6dd2b
Francis Tiangco <fptiangco@gmail.com>
2022-03-12 14:31:29 +0000

    Add test badge

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="stringtoolsfpat",
-    version="0.0.14",
+    version="0.0.15",
     packages=find_packages(),
     description="String tools",
     long_description=README_MD,

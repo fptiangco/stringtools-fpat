70bfb4fdb9e34a5f8b48edf9bfba57900a805b4d
Francis Tiangco <fptiangco@gmail.com>
2022-03-13 18:22:32 +0000

    Add black to workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="stringtoolsfpat",
-    version="0.0.25",
+    version="0.0.27",
     packages=find_packages(),
     description="String tools",
     long_description=README_MD,

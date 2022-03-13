16107f9257696c783ddc30da2699c1789b09f21c
Francis Tiangco <fptiangco@gmail.com>
2022-03-13 18:25:38 +0000

    Increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="stringtoolsfpat",
-    version="0.0.27",
+    version="0.0.28",
     packages=find_packages(),
     description="String tools",
     long_description=README_MD,

2f5d5a182f3f7148658178fc57f89d1d6528cc4b
Francis Tiangco <fptiangco@gmail.com>
2022-03-13 18:33:07 +0000

    Test black

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="stringtoolsfpat",
-    version="0.0.29",
+    version="0.0.30",
     packages=find_packages(),
     description="String tools",
     long_description=README_MD,

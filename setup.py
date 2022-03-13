4a67bab1e2cf13c454bda54e967777ca7b3b3a23
Francis Tiangco <fptiangco@gmail.com>
2022-03-13 18:10:08 +0000

    Update from black

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="stringtoolsfpat",
-    version="0.0.23",
+    version="0.0.25",
     packages=find_packages(),
     description="String tools",
     long_description=README_MD,
@@ -15,11 +15,11 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3 :: Only"
+        "Programming Language :: Python :: 3 :: Only",
     ],
     project_urls={
-        'Bug Reports': 'https://github.com/fptiangco/stringtoolsfpat/issues',
-        'Source': 'https://github.com/fptiangco/stringtoolsfpat/'
+        "Bug Reports": "https://github.com/fptiangco/stringtoolsfpat/issues",
+        "Source": "https://github.com/fptiangco/stringtoolsfpat/",
     },
     keywords="stringtoolsfpat",
 )

8e5297c78ac06fb9cc1ad46c61a172a521adde4c
Francis Tiangco <fptiangco@gmail.com>
2022-03-16 00:08:41 +0000

    Change theme

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'stringtoolsfpat'
-copyright = '2022, Francis Tiangco'
-author = 'Francis Tiangco'
+copyright = '2022, fpat'
+author = 'fpat'
 
 
 # -- General configuration ---------------------------------------------------
@@ -44,8 +44,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

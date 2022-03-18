272b9a426dc653f113cd5e6e21ebbc93d00b819b
Francis Tiangco <fptiangco@gmail.com>
2022-03-18 23:21:16 +0000

    Update index

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="stringtoolsfpat",
-    version="0.0.40",
+    version="0.0.41",
     packages=find_packages(),
     description="String tools",
     long_description=README_MD,

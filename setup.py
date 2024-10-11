dbc2823f1c1273de755397584055484acb5f647b
Francis Tiangco <fptiangco@gmail.com>
2024-10-12 00:14:06 +0100

    Restore readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="stringtoolsfpat",
-    version="0.0.67",
+    version="0.0.68",
     packages=find_packages(),
     description="String tools",
     long_description=README_MD,
